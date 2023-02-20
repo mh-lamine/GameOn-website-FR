a62241118f9aac28c1bd44e653b53967ac06163b
mh-lamine <mlamins.ngom@gmail.com>
2023-02-20 14:45:40 +0100

    validation date

--- js/modal.js
+++ js/modal.js
@@ -24,29 +24,29 @@ function launchModal() {
 
 const closeBtn = document.querySelector(".close");
 
-closeBtn.addEventListener("click", closeModal); 
+closeBtn.addEventListener("click", closeModal);
 
 function closeModal() {
-  modalbg.style.display = "none"; // ajoute le style display: none; à la classe modalbg
+  modalbg.style.display = "none";
 }
 
 // verification des champs
 
 function setError(element, message) {
-  const formData = element.parentElement; // prend la div parente de l'element
-  const errorMsg = formData.querySelector(".error"); // prend la div de classe error presente dans la div de l'element
-  errorMsg.innerText = message; // affiche le message d'erreur
+  const formData = element.parentElement;
+  const errorMsg = formData.querySelector(".error");
+  errorMsg.innerText = message;
 }
 
 function hideErrorMsg(element) {
-  const formData = element.parentElement; 
+  const formData = element.parentElement;
   const errorMsg = formData.querySelector(".error");
-  errorMsg.innerText = ""; // efface le mesqsage d'erreur
+  errorMsg.innerText = "";
 }
 
 function isValidEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase()); // verifie que le format de l'adresse mail est valide
+  return re.test(String(email).toLowerCase());
 }
 
 function validateInputs(){
@@ -54,7 +54,7 @@ function validateInputs(){
   const firstValue = document.getElementById("first").value.trim();
   const lastValue = document.getElementById("last").value.trim();
   const emailValue = document.getElementById("email").value.trim();
-  const birthdate = document.getElementById("birthdate");
+  const dob = document.getElementById("birthdate").value;
   const cu = document.getElementById("checkbox1");
     
   if (firstValue === "") {
@@ -81,6 +81,25 @@ function validateInputs(){
     errorCount++;
   }
 
+  if (dob == "") {
+    setError(birthdate, "Veuillez entrer une date de naissance");
+  } else {
+    const birthdateArray = dob.split("-");
+    console.log(birthdateArray[1]);
+    const currentYear = new Date().getFullYear(); 
+    // if (birthdateArray[2] == "") {
+    //   setError(birthdate, "nope")
+    //   console.log("nope")
+    // }
+    // if (birthdateArray[1] == "") {
+    //   setError(birthdate, "nope")
+    //   console.log("nope")
+    // }
+    if (parseInt(birthdateArray[0]) < 1923 || parseInt(birthdateArray[0]) > currentYear) {
+      setError(birthdate, "Veuillez entrer une date de naissance valide")
+    }
+  }
+    
   if (!cu.checked) {
     setError(cu, "Veuillez accepter les conditions d'utilisation");
     errorCount++;
@@ -92,10 +111,9 @@ function validateInputs(){
 }
 
 const form = document.getElementById("form").addEventListener("submit", e => {
-  e.preventDefault(); // bloque la soumission automatique du formulaire
+  e.preventDefault();
   if (validateInputs()){
-    document.getElementById("form-card").innerHTML = "Merci ! Votre réservation a été reçue."; 
-    // affiche le message de reussite si toutes les entrées sont valides
+    document.getElementById("form-card").innerHTML = "Merci ! Votre réservation a été reçue.";
   };
 
 });
